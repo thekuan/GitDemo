b1d3ddf1cc2b5356e1f0fb510d162cadfc1fad8b
Akash Shah <Aimee@akashs-macbook-pro.local>
2017-11-06 11:42:46 -0500

    Your 'gitLabel' was trash so I made a change

--- GitDemo/ViewController.swift
+++ GitDemo/ViewController.swift
@@ -14,6 +14,7 @@ class ViewController: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        gitLabel.textColor = UIColor.blue
         // Do any additional setup after loading the view, typically from a nib.
     }
 
